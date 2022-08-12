300010ba465bbb71b08f7d79758de4e91571c91c
Heshan <heshannamal222@gmail.com>
2022-08-12 10:00:53 +0530

    new commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,11 @@
             </div>
             <section class="login-clean">
                 <form method="post" action="{{ route('login') }}">
-				
+            @if(Session::get('success'))
+                <div class="alert alert-success">
+                    {{ Session::get('success') }}
+                </div>
+            @endif		
 			@if(Session::get('fail'))
                <div class="alert alert-danger">
                   {{ Session::get('fail') }}
